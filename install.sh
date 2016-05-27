f61bc3e7d7386f724af46c197d3f1a4e569cb476
Shangbin Yang <rccoder.net@gmail.com>
2016-05-27 16:43:33 +0800

    Add script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+cd ~
+mkdir shadowsocks_qt5
+cd shadowsocks_qt5
+sudo apt-get install git -y
+
+sudo apt-get install qt5-qmake qtbase5-dev libbotan1.10-dev -y
+git clone https://github.com/shadowsocks/libQtShadowsocks
+cd libQtShadowsocks
+dpkg-buildpackage -uc -us -b
+cd ..
+sudo dpkg -i libqtshadowsocks-dev*.deb
+sudo dpkg -i libqtshadowsocks_*.deb
+
+sudo apt-get install qt5-qmake qtbase5-dev libqrencode-dev libappindicator-dev libzbar-dev libbotan1.10-dev -y
+git clone https://github.com/shadowsocks/shadowsocks-qt5
+cd shadowsocks-qt5
+dpkg-buildpackage -uc -us -b
+cd ..
+sudo dpkg -i shadowsocks-libqtshadowsocks_*.deb
+sudo dpkg -i shadowsocks-qt5_*.deb
